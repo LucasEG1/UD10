8d36e4bb0d6bb7d1f9b8c3a616032c27db38cde6
casa <casa@Lucardio>
2022-04-08 13:38:19 +0200

    CREADA CLASE PARA PROBAR METODOS - Select from tabla

--- UD10/src/JDBCTests/JDBCPrimeraConexion.java
+++ UD10/src/JDBCTests/JDBCPrimeraConexion.java
@@ -21,38 +21,7 @@ public class JDBCPrimeraConexion {
             System.out.println(e.getMessage());
         }
         
-        try {
-            /*ESENCIAL PARA CONECTARSE A LA BASE DE DATOS*/
-            String url = "jdbc:mysql://localhost:3306/prueba";
-            String username = "root";
-            String password = "";
-
-            //CREAR CONEXION, STATEMENT, RESULT SET PARA USAR
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM persona");
-
-            while (rs.next()) {
-
-                int id = rs.getInt("id");
-                String nombre = rs.getString("nombre");
-                String apellido = rs.getString("apellido");
-                Date fecha = rs.getDate("fecha");
-
-                /* String.format permite imprimir cada columna con su valor correspondiente:
-                    %d para mostrar como int
-                    %s para mostrar como string
-                */
-                System.out.println(String.format("%d, %s %s, %s", id, nombre, apellido, fecha));
-            }
-
-            rs.close();
-            statement.close();
-            connection.close();
-
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
+        MetodosSQL.selectEnTabla("jugador");
 
         }
 }
--- UD10/src/JDBCTests/MetodosSQL.java
+++ UD10/src/JDBCTests/MetodosSQL.java
@@ -1,5 +1,6 @@
 package JDBCTests;
 
+import java.sql.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -13,8 +14,46 @@ public class MetodosSQL {
     // CLASE DE PRUEBA - NO TOCAR NADA
     // CLASE DE PRUEBA - NO TOCAR NADA
     // CLASE DE PRUEBA - NO TOCAR NADA
-    
-    public static void crearTabla() {
+    public static void selectEnTabla(String tabla) {
+
+        /*ESENCIAL PARA CONECTARSE A LA BASE DE DATOS*/
+        try {
+            String url = "jdbc:mysql://localhost:3306/scoreboard";
+            String username = "root";
+            String password = "";
+
+            //CREAR CONEXION, STATEMENT, RESULT SET PARA USAR
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM " + tabla);
+
+            while (rs.next()) {
+
+                String nJugador = rs.getString("username");
+                int kills = rs.getInt("kills");
+                int deaths = rs.getInt("deaths");
+                boolean mvp = rs.getBoolean("mvp");
+
+                /* String.format permite imprimir cada columna con su valor correspondiente:
+                    %d para mostrar como int
+                    %s para mostrar como string
+                 */
+                System.out.println(String.format("%s, %d, %d, %s", nJugador, kills, deaths, mvp));
+            }
+
+            rs.close();
+            statement.close();
+            connection.close();
+            
+            
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public static void crearTabla() {
         System.out.println("Nombre de la tabla:");
         String nTabla = leer.nextLine();
 
@@ -32,7 +71,7 @@ public class MetodosSQL {
         }
 
         for (int i = 0; i < nCols; i++) {
-            System.out.println("¿Nombre de la columna?");
+            System.out.println("¿Nombre de la columna " + i + "?");
             String nomCol = leer.nextLine();
 
             System.out.println("¿Tipo de dato? (Ejemplo: int, boolean, varchar(número de caracteres)...");
@@ -42,18 +81,37 @@ public class MetodosSQL {
             String nulo = leer.nextLine();
             
             System.out.println("¿Es clave primaria? s/n");
+            boolean esPrimaria = false;
             String pk = leer.nextLine();
             char Pk = pk.charAt(0);
             switch (Pk) {
-                case 'y': case 'Y':
-                                        
+                case 's': case 'S':
+                    esPrimaria = true;
                     break;
                 case 'n': case 'N':
+                    esPrimaria = false;
                     break;
                 default:
                     System.out.println("Se debe especificar si es clave primaria o no.");  
             }
             
+            //ESENCIAL PARA CONECTARSE A LA BASE DE DATOS
+            try {
+                String url = "jdbc:mysql://localhost:3306/";
+                String username = "root";
+                String password = "";
+
+                //CREAR CONEXION, STATEMENT, RESULT SET PARA USAR
+                Connection connection = DriverManager.getConnection(url, username, password);
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("Create table if not exists " + nTabla + "("
+                        + "");
+            } catch (SQLException sqle) {
+                System.out.println(sqle.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
         }
-    }
+    }*/
 }
