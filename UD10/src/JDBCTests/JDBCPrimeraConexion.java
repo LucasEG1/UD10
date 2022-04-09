c3f4e6679719cacc1f5d7e723d7c74223b074ac7
casa <casa@Lucardio>
2022-04-09 13:36:28 +0200

    limpiado espacios

--- UD10/src/JDBCTests/JDBCPrimeraConexion.java
+++ UD10/src/JDBCTests/JDBCPrimeraConexion.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JDBCTests;
 
-import java.sql.*;
-
 /**
- *
+ * 
  * @author casa
  */
 public class JDBCPrimeraConexion {
@@ -20,6 +13,7 @@ public class JDBCPrimeraConexion {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+          
         
         MetodosSQL.selectEnTabla("jugador");
         System.out.println("---------");
